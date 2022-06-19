904e20452a7d5514b5232288cf75ddea03e77dec
Yifang Tsai <yifangtsai@gmail.com>
2022-06-19 14:42:55 +0800

    assign mid into while loop

--- Week-2/Assignment-5/ass5.js
+++ Week-2/Assignment-5/ass5.js
@@ -1,20 +1,20 @@
 function binarySearchPosition(numbers, target) {
-   let lt = 0, rt = numbers.length - 1, mid = (lt + rt) / 2 ; 
+   let lt = 0, rt = numbers.length - 1;
    while (lt <= rt) {
-      mid = (lt + rt) >> 1; 
+      let mid = (lt + rt) >> 1;
       if (target == numbers[mid]) {
          return mid
       }
       else if (target < numbers[mid]) {
-         rt = mid -1
+         rt = mid - 1
       }
       else {
          lt = mid + 1
       }
    }
-    
-   }
-console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); 
+
+}
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 1));
 // should print 0
-console.log(binarySearchPosition([1, 2, 5, 6, 7], 6)); 
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 6));
 // should print 3
