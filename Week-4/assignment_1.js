f39f40e66dcf5a669609bd89f11b8dc0f016ced7
Yifang Tsai <yifangtsai@gmail.com>
2022-07-04 17:47:52 +0800

    add another method for W4 assi1

--- Week-4/assignment_1.js
+++ Week-4/assignment_1.js
@@ -1,3 +1,4 @@
+/* method 1*/
 function delayedResult(n1, n2, delayTime, callback) {
     let sum = n1 + n2;
     setTimeout(() => {
@@ -6,8 +7,25 @@ function delayedResult(n1, n2, delayTime, callback) {
 }
 delayedResult(4, 5, 3000, function (result) {
     console.log(result);
-}); // 9 (4+5) will be shown in the console after 3 seconds
+});
+// 9 (4+5) will be shown in the console after 3 seconds
 
 delayedResult(-5, 10, 2000, function (result) {
     console.log(result);
-}); // 5 (-5+10) will be shown in the console after 2 seconds
+});
+
+// 5 (-5+10) will be shown in the console after 2 seconds
+
+
+/* method 2*/
+
+function delayedResult(n1, n2, delayTime) {
+    let sum = n1 + n2;
+    setTimeout(() => {
+        console.log(sum);
+    }, delayTime)
+};
+
+delayedResult(5, 5, 5000)
+
+delayedResult(6, 6, 6000)
